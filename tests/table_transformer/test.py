e68e5ed0824d1b08e2fe10af0a5bde6c469163e3
zhouksh <zhouksh@gmail.com>
2025-03-31 22:24:36 +0800

    update

--- tests/table_transformer/test.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoImageProcessor
-
-
-processor = AutoImageProcessor.from_pretrained("microsoft/table-transformer-detection")
-print(type(processor))
-
-processor.save_pretrained("./ckpt/original")
